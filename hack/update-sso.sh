463296e923343a7b93970bba39ee1a9cbca2aa18
John Duimovich <jduimovich@gmail.com>
2023-01-20 15:49:13 -0500

    dev-sso auth domain

--- hack/update-sso.sh
+++ hack/update-sso.sh
@@ -4,19 +4,20 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 
 SSO_HOST_KEYCLOAK=$( kubectl get routes keycloak -n dev-sso  -o jsonpath={".spec.host"})
 DOMAIN=$(kubectl  get ingresses.config.openshift.io cluster -o jsonpath={".spec.domain"})
-echo "DOMAIN AUTH is: $SSO_HOST_KEYCLOAK"
+echo "DOMAIN AUTH is: $DOMAIN"
 echo "KEYCLOAK AUTH is: $SSO_HOST_KEYCLOAK"
 
-if [ "$DOMAIN" == "apps-crc.testing"]; then
+if [ "$DOMAIN" == "apps-crc.testing" ]; then
   DOMAIN="env-boot-local-127-0-0-1.nip.io"
 fi 
 
-cat << EOF > components/hac-boot/environments
+cat << EOF > components/hac-boot/environment.yaml
 apiVersion: cloud.redhat.com/v1alpha1
 kind: FrontendEnvironment
 metadata:
   name: env-boot
 spec: 
+# Use hack/update-sso.sh to compute this for dev-sso auth domain 
   sso: "https://$SSO_HOST_KEYCLOAK/auth/" 
   ingressClass: openshift-default
   hostname: $DOMAIN
